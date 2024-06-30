6edb819a2bc05f2fe88210174b44806a96533c36
Arnon Lutsky <realsplashingfish@gmail.com>
2024-06-30 13:17:25 +0300

    a

--- EX2/copytree.c
+++ EX2/copytree.c
@@ -3,42 +3,94 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <stdio.h>
+
+void get_directory_path(const char *file_path, char *dir_path)
+{
+    const char *last_slash = strrchr(file_path, '/');
+    if (last_slash == NULL)
+    {
+        strcpy(dir_path, ".");
+    }
+    else
+    {
+        size_t dir_length = last_slash - file_path + 1;
+        strncpy(dir_path, file_path, dir_length);
+        dir_path[dir_length] = '\0';
+    }
+}
 
 void copy_file(const char *src, const char *dest, int copy_symlinks, int copy_permissions)
 {
-    struct dirent *entry;
-    DIR *dp;
     struct stat st;
-    if (copy_symlinks)
+
+    if (lstat(src, &st) == -1)
     {
-        if (!lstat(src, st))
+        perror("lstat error");
+        return;
+    }
+
+    if (S_ISLNK(st.st_mode))
+    {
+        char linked_file[st.st_size + 1];
+        ssize_t len = readlink(src, linked_file, sizeof(linked_file) - 1);
+        if (len < 0)
         {
-            if (S_ISLNK(st.st_mode))
-            {
-                char linked_file[strlen(src) + 1];
-                ssize_t len = readlink(src, linked_file, sizeof(linked_file) - 1);
-                if (len < 0)
-                {
-                    perror("readlink error");
-                    return;
-                }
-                else
-                {
-                    linked_file[len] = "\0";
-                    if (symlink(linked_file, dest) < 0)
-                    {
-                        perror("symlink error");
-                    }
-                    return;
-                }
-            }
+            perror("readlink error");
+            return;
         }
         else
         {
-            perror("lstat error");
-            return;
+            linked_file[len] = '\0';
+            if (copy_symlinks)
+            {
+                if (symlink(linked_file, dest) < 0)
+                {
+                    perror("symlink error");
+                }
+                return;
+            }
+            else
+            {
+                char dir_path[PATH_MAX];
+                get_directory_path(src, dir_path);
+                char full_path[PATH_MAX];
+                snprintf(full_path, sizeof(full_path), "%s%s", dir_path, linked_file);
+
+                const char *file_name = strrchr(linked_file, '/');
+                if (file_name == NULL)
+                {
+                    file_name = linked_file;
+                }
+                else
+                {
+                    file_name++;
+                }
+                char dest_path[PATH_MAX];
+                get_directory_path(dest, dest_path);
+                char new_dest[PATH_MAX];
+                snprintf(new_dest, sizeof(new_dest), "%s%s", dest_path, file_name);
+
+                if (!link(full_path, new_dest))
+                {
+                    if (copy_permissions)
+                    {
+                        if (chmod(dest, st.st_mode) == -1)
+                        {
+                            perror("chmod failed");
+                        }
+                    }
+                }
+                else
+                {
+                    perror("link error");
+                    return;
+                }
+                return;
+            }
         }
     }
+
     if (!link(src, dest))
     {
         if (copy_permissions)
@@ -55,11 +107,13 @@ void copy_file(const char *src, const char *dest, int copy_symlinks, int copy_pe
         return;
     }
 }
+
 void create_directory(const char *dir_name)
 {
     int len = strlen(dir_name);
-    char dir[len];
+    char dir[len + 1];
     struct stat st = {0};
+
     strcpy(dir, dir_name);
     if (dir[len - 1] == '/')
     {
@@ -70,7 +124,6 @@ void create_directory(const char *dir_name)
         if (*p == '/')
         {
             *p = 0;
-
             if (stat(dir, &st) == -1)
             {
                 if (mkdir(dir, S_IRWXU) != 0)
@@ -79,7 +132,6 @@ void create_directory(const char *dir_name)
                     return;
                 }
             }
-
             *p = '/';
         }
     }
@@ -92,16 +144,35 @@ void create_directory(const char *dir_name)
         }
     }
 }
+
 void copy_directory(const char *src, const char *dest, int copy_symlinks, int copy_permissions)
 {
     struct dirent *entry;
     DIR *dp;
     struct stat st;
+    struct stat st_tmp;
+    DIR *dir_tmp = opendir(dest);
+    if (dir_tmp)
+    {
+        if ((stat(dest, &st_tmp) == 0))
+        {
+            struct dirent *entry;
+            while ((entry = readdir(dir_tmp)) != NULL)
+            {
+                if (entry->d_name[0] != '.' || (entry->d_name[1] != '\0' && (entry->d_name[1] != '.' || entry->d_name[2] != '\0')))
+                {
+                    fprintf(stderr, "Directory is not empty");
+                    return;
+                }
+            }
+        }
+        closedir(dir_tmp);
+    }
     dp = opendir(src);
     if (dp == NULL)
     {
         perror("opendir error");
-        return -1;
+        return;
     }
     create_directory(dest);
     while ((entry = readdir(dp)) != NULL)
@@ -110,7 +181,8 @@ void copy_directory(const char *src, const char *dest, int copy_symlinks, int co
         {
             continue;
         }
-        char *src_path, dest_path;
+        char src_path[PATH_MAX + 1];
+        char dest_path[PATH_MAX + 1];
         snprintf(src_path, sizeof(src_path), "%s/%s", src, entry->d_name);
         snprintf(dest_path, sizeof(dest_path), "%s/%s", dest, entry->d_name);
         if (!lstat(src_path, &st))
